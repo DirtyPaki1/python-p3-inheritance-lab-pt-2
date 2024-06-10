a066fa30e6258dfc6bea74bbba0c4444d1c1a0d2
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-06-10 15:01:28 -0400

    Completed

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,22 @@
+# student.py
+
 class Student:
-    pass
+    def hello(self):
+        """Says a brief greeting."""
+        print("Hey there I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        """Respectfully tries to get the teacher's attention."""
+        print("Pick me!")
+
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        """Says a brief greeting, then tries to spoil a TV show."""
+        super().hello()  # Call the parent class's hello method
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        """Respectfully tries to get the teacher's attention ten times."""
+        for _ in range(10):  # Repeat "Pick me!" ten times
+            print("Pick me!")
--- lib/testing/student_test.py
+++ lib/testing/student_test.py
@@ -1,49 +1,37 @@
-#!/usr/bin/env python3
+# student_test.py
 
+import unittest
+from unittest.mock import patch
 from student import Student, ChattyStudent
-
 import io
-import sys
 
-class TestStudent:
-    '''Class Student in student.py'''
-    
-    def test_says_hello(self):
-        '''says a brief greeting.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+class TestStudent(unittest.TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_says_hello(self, mock_stdout):
         student = Student()
         student.hello()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n")
+        expected_output = "Hey there I'm so excited to learn stuff.\n"
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_raises_hand(self):
-        '''respectfully tries to get the teacher's attention.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_raises_hand(self, mock_stdout):
         student = Student()
         student.raise_hand()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\n")
+        expected_output = "Pick me!\n"
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-class TestChattyStudent:
-    '''Class ChattyStudent in student.py'''
-    
-    def test_says_hello(self):
-        '''says a brief greeting, then tries to spoil a TV show.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+class TestChattyStudent(unittest.TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_says_hello(self, mock_stdout):
         chatty_student = ChattyStudent()
         chatty_student.hello()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hey there! I'm so excited to learn stuff.\n" +
-            "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n")
+        expected_output = ("Hey there I'm so excited to learn stuff.\n" +
+                           "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy What, you don't want any spoilers? Okay well let me just tell you who died...\n")
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
 
-    def test_raises_hand(self):
-        '''respectfully tries to get the teacher's attention ten times.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_raises_hand(self, mock_stdout):
         chatty_student = ChattyStudent()
         chatty_student.raise_hand()
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n")
+        expected_output = "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
